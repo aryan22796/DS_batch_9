dde37e66c711bfafb3ef5d442b0564cfa58a8f03
Nagendra Yadav <49116792+aryan22796@users.noreply.github.com>
2023-08-14 22:22:08 +0530

    updata

--- Day-1/python/python_day1.py
+++ Day-1/python/python_day1.py
@@ -1,1 +1,10 @@
-print("helo this is demo  class of python")
+"""
+write the basic python code , for adding two number or arthmatic operation 
+"""
+
+var_a= 4 #string data type 
+var_b= 4 #int data type 
+output= var_a+var_b
+print("the result is  : - ", output)
+print("the type of var_a is  : - ", type(var_a))
+print("the type of var_B is  : - ", type("4"))
